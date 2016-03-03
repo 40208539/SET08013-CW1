88f7fa1f4d344a7ed45a6b890e6712d697cf94dc
40208539 <40208539@live.napier.ac.uk>
2016-03-03 12:11:21 +0000

    added Message class

---
+++ SET08013 CW1/SET08013 CW1/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SET08013_CW1
+{
+    class Message
+    {
+    }
+}
